a0d309cb758e6a0d64931f2a9c8cfc3b8d273c82
Yahli Mariansky <yahlimem@gmail.com>
2023-03-24 14:35:00 +0300

    ball are bouncin!!!

--- src/ball.rs
+++ src/ball.rs
@@ -1,5 +1,6 @@
+use std::ops::{Add, Div, Mul};
 use nannou::prelude::*;
-use crate::{GRAVITY, Model};
+use crate::{GRAVITY};
 
 #[derive(Copy, Clone)]
 pub struct Ball{
@@ -9,6 +10,7 @@ pub struct Ball{
     pub mass: f32,
     pub velocity: Vec2,
     pub acceleration: Vec2,
+    pub bouncy: f32,
 
 }
 
@@ -21,29 +23,32 @@ impl Ball {
         }
     }
 
-    pub fn update(&mut self, ball_array: &mut Vec<Ball>) {
+    pub fn  update(&mut self) {
         self.update_movement();
         self.add_gravity();
-        let border_collision = self.bound_col(600.0, 400.0);
+        let border_collision = self.bound_col(800.0, 450.0);
         if border_collision.0 {
-            self.velocity *= -border_collision.1.abs();
+            while self.bound_col(800.0, 450.0).0 {
+                self.pos -= self.bound_col(800.0, 450.0).1;
+            }
+            if border_collision.1.x.abs() == 1.0 {
+                self.velocity.x *= -1.0 * 0.99;
+            } else if border_collision.1.y.abs() == 1.0 {
+                self.velocity.y *= -1.0 * 0.99;
+            }
+            // self.velocity *= -border_collision.1.abs().add(vec2(1.0,1.0)).normalize() * 0.89;
         }
-        let circles_collision = self.check_collision(&ball_array.clone());
-        if circles_collision.0 {
-            self.velocity = circles_collision.1;
-        }
-
     }
 
 
 }
 
 impl Ball {
-    pub fn bound_col(&mut self, width: f32, height: f32,) -> (bool, Vec2){
+    pub fn bound_col(&self, width: f32, height: f32,) -> (bool, Vec2){
         let right = width/2.0 - self.radius;
-        let left = -width/2.0 - self.radius;
+        let left = -width/2.0 + self.radius;
         let top = height/2.0 - self.radius;
-        let bottom = -height/2.0 - self.radius;
+        let bottom = -height/2.0 + self.radius;
 
         if self.pos.x < left {
             return (true, vec2(-1.0,0.0));
@@ -57,27 +62,32 @@ impl Ball {
         return (false, vec2(1.0,1.0));
     }
 
-    pub fn check_collision(&mut self, ball_array: &Vec<Ball>) -> (bool, Vec2) {
+    pub fn check_collision(&mut self, ball_array: &Vec<Ball>) -> (bool, Vec2, Vec2) {
         for i in ball_array{
             if i.pos == self.pos {
                 continue;
             }
             let d = ((self.pos.x - i.pos.x) * (self.pos.x - i.pos.x) + (self.pos.y - i.pos.y) * (self.pos.y - i.pos.y)).sqrt();
             if (i.radius - self.radius).abs() <= d && d <= (i.radius + self.radius).abs(){
-                return (true, vec2((self.pos.x - i.pos.x), (self.pos.y - i.pos.y)));
+                // let force_vec: Vec2 = (self.velocity.mul(self.mass) + i.velocity.mul(i.mass)).div(self.mass + i.mass);
+                // let force = (force_vec.x * force_vec.x + force_vec.y * force_vec.y).sqrt();
+                // let normal_vector = (self.pos - i.pos).normalize().mul(force);
+                let dir = (i.pos - self.pos).normalize();
+                let dir_out = (self.pos - i.pos).normalize().mul(self.velocity.length());
+                return (true, dir_out, dir);
             }
         }
-        return (false, vec2(0.0, 0.0));
+        return (false, vec2(0.0, 0.0), vec2(0.0, 0.0));
     }
 }
 
 impl Ball {
     pub fn update_movement(&mut self) {
-        self.velocity += self.acceleration;
-        self.pos += self.velocity;
+        self.velocity += self.acceleration * 0.1;
+        self.pos += self.velocity * 0.1;
     }
 
     fn add_gravity(&mut self) {
-        self.velocity.y += GRAVITY;
+        self.velocity.y += GRAVITY / 10.0;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod ball;
+
 use crate::ball::Ball;
 use nannou::prelude::*;
+use rand::Rng;
 
 const NUM_BALLS: u32 = 10;
 static GRAVITY: f32 = -9.8;
@@ -11,7 +13,10 @@ struct Model{
 }
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model)
+        .update(update)
+        .size(800, 450)
+        .run();
 }
 
 fn model(app: &App) -> Model {
@@ -19,12 +24,12 @@ fn model(app: &App) -> Model {
     let mut ball_array = vec![];
     for i in 0..NUM_BALLS {
         ball_array.push(Ball {
-            pos: vec2((i * 40) as f32, 0.0),
+            pos: vec2(rand::thread_rng().gen_range(0..100) as f32, (i * 40) as f32),
             radius: 20.0,
             mass: 10.0,
             velocity: vec2(0.0,0.0),
             acceleration: vec2(0.0,0.0),
-            id: i,
+            bouncy: 1.0
         });
     }
 
@@ -35,8 +40,16 @@ fn model(app: &App) -> Model {
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    for ball in 0..model.ball_array.len() - 1 {
-        model.ball_array[ball].update(&mut model.ball_array[ball + 1]);
+    for ball in 0..model.ball_array.len() {
+        model.ball_array[ball].update();
+        let circles_collision = model.ball_array[ball].clone().check_collision(&model.ball_array.clone());
+        if circles_collision.0 {
+            // while model.ball_array[ball].clone().check_collision(&model.ball_array.clone()).0 {
+            //     let go_dir = model.ball_array[ball].clone().check_collision(&model.ball_array.clone()).2;
+            //     model.ball_array[ball].pos -= go_dir;
+            // }
+            model.ball_array[ball].velocity = circles_collision.1;
+        }
     }
 
 }
